0b486d971bf680926efc4e1f11aa894e4744c2a6
Artem <skriptoff@gmail.com>
2018-05-10 23:27:33 +0300

    Use react app and make some HW

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
- import React from "react";
- import ReactDOM from "react-dom";
- import Game from "./App.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+import Game from "./App.js";
+import Header from "./React.js";
 
+ReactDOM.render(<Header />, document.getElementById("home"));
 ReactDOM.render(<Game />, document.getElementById("root"));
+//registerServiceWorker();
